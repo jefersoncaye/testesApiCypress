c5d84f074f149c4f0b3526268ae05b5d4331cb74
Jeferson Caye <jefercaye@yahoo.com.br>
2022-07-20 21:11:03 -0300

    Adicionando Reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
     baseUrl: 'https://fakerestapi.azurewebsites.net/api/v1/',
-    video: false
+    video: false,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      overwrite: false,
+      json: false,
+      html: true,
+      timestamp: 'dd-mm-yyyy_(HH-MM)',
+      reportFilename: "[status]-[datetime]-[name]",
+    }
   },
 });
